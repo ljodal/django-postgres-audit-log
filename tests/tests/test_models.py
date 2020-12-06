433069d463a4fcd70f3947746e5255cf4a040d77
Sigurd Ljødal <544451+ljodal@users.noreply.github.com>
2020-12-06 03:19:39 +0100

    Add tests for prefetching

--- tests/tests/test_models.py
+++ tests/tests/test_models.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 import pytest
 from django.db import connection
 
@@ -150,3 +152,65 @@ def test_sql_delete_is_audit_logged() -> None:
     assert log_entry.action == "DELETE"
     assert log_entry.changes == {"id": model_id, "some_text": "Some text"}
     assert log_entry.log_object is None
+
+
+@pytest.mark.usefixtures("db", "audit_logging_context")
+def test_prefetch_audit_logged_object(django_assert_num_queries: Callable) -> None:
+    """
+    Test that the audit logging context manager works and that we can insert
+    data, and that the insert is audit logged.
+    """
+
+    first_model = MyAuditLoggedModel.objects.create(some_text="Some text")
+    second_model = MyAuditLoggedModel.objects.create(some_text="Some other text")
+
+    audit_logs = AuditLogEntry.objects.order_by("id")
+
+    # Test without prefetch, should generate 3 queries
+    with django_assert_num_queries(3):
+        assert len(audit_logs) == 2
+
+        first, second = audit_logs
+        assert first.log_object == first_model
+        assert second.log_object == second_model
+
+    # Update queryset to include prefetch
+    audit_logs = audit_logs.prefetch_related("log_object")
+
+    with django_assert_num_queries(2):
+        assert len(audit_logs) == 2
+
+        first, second = audit_logs
+        assert first.log_object == first_model
+        assert second.log_object == second_model
+
+
+@pytest.mark.usefixtures("db", "audit_logging_context")
+def test_prefetch_log_entries(django_assert_num_queries: Callable) -> None:
+    """
+    Test that the audit logging context manager works and that we can insert
+    data, and that the insert is audit logged.
+    """
+
+    # Create two objects and then update both, generating 4 log entries
+    MyAuditLoggedModel.objects.create(some_text="Some text")
+    MyAuditLoggedModel.objects.create(some_text="Some text")
+    MyAuditLoggedModel.objects.update(some_text="Some other text")
+
+    assert AuditLogEntry.objects.count() == 4
+
+    models = MyAuditLoggedModel.objects.order_by("id")
+
+    # Test without prefetch, should generate 3 queries
+    with django_assert_num_queries(3):
+        for model in models:
+            audit_logs = model.audit_logs.all()
+            assert len(audit_logs) == 2
+
+    # Update queryset to include prefetch
+    audit_logs = models.prefetch_related("audit_logs")
+
+    with django_assert_num_queries(2):
+        for model in models:
+            audit_logs = model.audit_logs.all()
+            assert len(audit_logs) == 2
